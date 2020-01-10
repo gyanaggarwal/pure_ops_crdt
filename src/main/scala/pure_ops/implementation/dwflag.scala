f3f8a00f08e19fef626a61f604ee0503f157b797
Gyanendra Aggarwal <gyanendra.aggarwal@gmail.com>
2020-01-09 20:10:10 -0600

    Update dwflag.scala

--- src/main/scala/pure_ops/implementation/dwflag.scala
+++ src/main/scala/pure_ops/implementation/dwflag.scala
@@ -2,9 +2,9 @@ package pure_ops
 package implementation
 
 final case object DWFlag extends OWFlag {	
-	val init_data: Boolean = true
+  val init_data: Boolean = true
 	
-	def combine_any(value0: Any,
-								  value1: Any):
-	Any = value0.asInstanceOf[Boolean] && value1.asInstanceOf[Boolean]
+  def combine_any(value0: Any,
+		  value1: Any):
+  Any = value0.asInstanceOf[Boolean] && value1.asInstanceOf[Boolean]
 }
