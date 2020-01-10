da48fb2f3c445e8ebe45d8933000b83d3f51653c
Gyanendra Aggarwal <gyanendra.aggarwal@gmail.com>
2020-01-09 18:20:49 -0600

    Update awset.scala

--- src/main/scala/pure_ops/implementation/awset.scala
+++ src/main/scala/pure_ops/implementation/awset.scala
@@ -2,11 +2,11 @@ package pure_ops
 package implementation
 
 final case object AWSet extends OWSet {
-	def combine_ARSet(ar_set0: ARSet,
-	                  ar_set1: ARSet):
-	ARSet = {
-		val aset = ar_set0.aset ++ ar_set1.aset
-		val rset = (ar_set0.rset ++ ar_set1.rset) -- aset
-		ARSet(aset, rset)		
-	}
+  def combine_ARSet(ar_set0: ARSet,
+	            ar_set1: ARSet):
+  ARSet = {
+    val aset = ar_set0.aset ++ ar_set1.aset
+    val rset = (ar_set0.rset ++ ar_set1.rset) -- aset
+    ARSet(aset, rset)		
+  }
 }
